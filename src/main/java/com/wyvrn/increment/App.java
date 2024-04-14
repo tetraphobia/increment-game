bcb5c860b2f6179044dea2cfe79014356da64922
Jacob Teddy <tetraphobia@wyvrn.dev>
2024-04-14 01:36:45 -0400

    build: package dependencies into jarfile

--- src/main/java/com/wyvrn/increment/App.java
+++ src/main/java/com/wyvrn/increment/App.java
@@ -1,11 +1,31 @@
 package com.wyvrn.increment;
 
+import java.io.IOException;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+        Screen screen = new TerminalScreen(terminal);
+
+        String s = "Hello world!";
+        TextGraphics tGraphics = screen.newTextGraphics();
+
+        screen.startScreen();
+        screen.clear();
+
+        tGraphics.putString(10, 10, s);
+        screen.refresh();
+
+        screen.readInput();
+        screen.stopScreen();
     }
 }
