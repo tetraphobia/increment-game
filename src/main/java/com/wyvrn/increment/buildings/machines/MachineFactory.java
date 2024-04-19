5f80bfa233e2579ef443d935a54871706254b1d3
Jacob Teddy <tetraphobia@wyvrn.dev>
2024-04-19 05:20:45 -0400

    docs: document MachineFactory

--- src/main/java/com/wyvrn/increment/buildings/machines/MachineFactory.java
+++ src/main/java/com/wyvrn/increment/buildings/machines/MachineFactory.java
@@ -10,6 +10,11 @@ public class MachineFactory {
     public MachineFactory() {
     }
 
+    /**
+     * @param type The type of machine ('small' | 'large'). Defaults to 'small' if
+     *             entry cannot be found.
+     * @return  {@link Machine}
+     */
     public Machine create(String type) {
         if (type.equals("small")) {
             return new SmallMachine();
